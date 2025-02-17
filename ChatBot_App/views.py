0e6c9e8270f34e48f7a866bc6796d8068e024495
PiyushJain045 <lightningiit123@@gmail.com>
2025-02-17 12:25:19 +0530

    add htmx

--- ChatBot_App/views.py
+++ ChatBot_App/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, JsonResponse
 from django.views import View
+import markdown
+
+import google.generativeai as genai
+import os
+from dotenv import load_dotenv
+load_dotenv() 
+
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 # Create your views here.
 
@@ -17,4 +26,18 @@ class Chat(View):
         return render(request, "chat.html")
 
     def post(self, request):
-        return render(request, "home.html")
+        prompt = request.POST.get("field")
+        print(prompt)
+
+        response = model.generate_content(prompt)
+        bot_response = markdown.markdown(response.candidates[0].content.parts[0].text)
+        print(bot_response)
+
+        chats = [
+            {"role": "user", "text": prompt},
+            {"role": "bot", "text": bot_response},
+            
+            
+        ]
+
+        return render(request, "chat.html", {"chats":chats})
